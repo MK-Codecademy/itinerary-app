18dc6a2126829a33269245075492f120049ec777
ChrisCooper0 <christopher.cooper@mail.com>
2022-04-29 21:12:57 +0100

    feat: add simple hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div className="h-52 w-full bg-slate-400">
+      <div className="h-full w-full flex justify-center items-center">
+        <h1 className="text-5xl text-slate-800">Itinerary App</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
