b47a9560a3aa6e97934afeec802c56231c8fac13
Will Garrett <willgarrett64@yahoo.com>
2022-04-29 17:31:27 +0100

    feat: add navbar basics

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="border border-solid h-20 w-full">
+    <div>Logo</div>
+    <button>Log In</button>
+    <button>Sign Up</button>
+  </div>;
+}
+
+export default Navbar;
