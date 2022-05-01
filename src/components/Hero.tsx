99006989d36da2b33cf6e451079164153c27c405
ChrisCooper0 <christopher.cooper@mail.com>
2022-05-01 13:36:01 +0100

    refactor: colour and App.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
-    <div className="h-52 w-full bg-slate-400">
+    <div className="h-52 w-full bg-purple-700">
       <div className="h-full w-full flex justify-center items-center">
-        <h1 className="text-5xl text-slate-800">Itinerary App</h1>
+        <h1 className="text-5xl text-purple-100">Itinerary App</h1>
       </div>
     </div>
   );
