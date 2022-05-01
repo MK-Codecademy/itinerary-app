cec734050e54d00c6e14e5fc4cce0e0f628bb75a
ChrisCooper0 <christopher.cooper@mail.com>
2022-05-01 13:36:01 +0100

    refactor: colour and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Hero from "./components/Hero";
 
 function App() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
 
 export default App;
