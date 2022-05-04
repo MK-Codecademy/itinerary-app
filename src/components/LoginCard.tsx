c767febb092b897ed7909175d7286865838b11f2
ChrisCooper0 <christopher.cooper@mail.com>
2022-05-04 20:47:26 +0100

    new login landing card

---
+++ src/components/LoginCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+
+const LoginCard = () => {
+  return (
+    <div className="card w-96">
+      <div className="card-content ">
+        <h3 className="card-header pt-10">Check out your plan</h3>
+        <div className="pb-10">
+          <Button text="Login" color="purple" filled />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginCard;
